58d67b11cb8607bf232131ccbe5a496754f2c252
Havin Mickel <12345678900qw@gmail.com>
2022-04-22 09:59:21 -0700

    Added ls command

--- bash.js
+++ bash.js
@@ -1,18 +1,21 @@
 // Output a prompt
+const pwd = require('./pwd')
+const ls = require('./ls')
 process.stdout.write('prompt > ');
 
 // The stdin 'data' even fires after a user types in a line
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); // remove the newLine
 
-
-
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd())
+  if(cmd === 'pwd'){
+    pwd()
+    process.stdout.write('\nprompt > ');
+  } else if(cmd === 'ls'){
+    ls()
   } else {
     process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
   }
 
-  process.stdout.write('\nprompt > ');
 });
 
