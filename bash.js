973225b272e33885666e60bc3862481fa5aaf2e9
kwangric <43795498+kwangric@users.noreply.github.com>
2022-04-22 12:24:59 -0400

    added pwd method

---
+++ bash.js
@@ -0,0 +1,18 @@
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' even fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); // remove the newLine
+
+
+
+  if (cmd === 'pwd') {
+    process.stdout.write(process.cwd())
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+  }
+
+  process.stdout.write('\nprompt > ');
+});
+
